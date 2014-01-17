475fd7d65f665e9b44c913b84dc9b3d658d20e10
Travis Beck <tjbenator@gmail.com>
2014-01-17 12:14:06 -0800

    Fixed invalid class names.

--- includes/ListDirectory.php
+++ includes/ListDirectory.php
@@ -35,7 +35,7 @@ class ListDirectory {
 		foreach($columns as $key => $col) {
 			//if(!in_array($name, $columns)) continue;
 			if(!isset($this->columns[$col])) continue;
-			echo "\t\t\t<th class='$col'>" . $col . "</th>\n";
+			echo "\t\t\t<th class='" . str_replace(" ", "-", $col) . "'>" . $col . "</th>\n";
 		}
 		echo "\t\t</tr>\n";
 		echo "\t</thead>\n\n";
@@ -49,9 +49,9 @@ class ListDirectory {
 				$func = $this->columns[$col];
 				$r = $func($dir);
 				if (isset($r['customkey'])) {
-					echo "\t\t\t<td class='$col' sorttable_customkey='" . $r['customkey'] . "'>";
+					echo "\t\t\t<td class='" . str_replace(" ", "-", $col) . "' sorttable_customkey='" . $r['customkey'] . "'>";
 				} else {
-					echo "\t\t\t<td class='$col'>";
+					echo "\t\t\t<td class='" . str_replace(" ", "-", $col) . "'>";
 				}
 				echo $r['data'] . "</td>\n";
 			}
